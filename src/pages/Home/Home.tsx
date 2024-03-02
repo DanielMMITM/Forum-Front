361c366ee6953f0d9cd71018d17d0b4f381a0911
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-02 00:56:08 -0600

    Add - add info to the home header and change specific typography

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,6 +5,13 @@ export const Home = () => {
     <>
       <Box display={"flex"} component={"header"} className="home-header">
         <h1 className="heading-primary home-header--heading">Welcome</h1>
+        <p className="home-header--text">
+          Nuestro foro es el lugar perfecto para compartir conocimientos y
+          resolver dudas.
+        </p>
+        <a href="#" className="home-header--swipe">
+          Explorar
+        </a>
       </Box>
       <Container>
         <Box
