9b76b8684113db84a16c6d8f723e69c55ee166ff
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-09 19:42:10 -0600

    Update home import

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PrivateRouter from "./PrivateRouter";
-import { Home } from "@/pages/Home/Home";
+import { Home } from "@/pages/Home";
 import { NavBar } from "@/components/NavBar";
 import { Footer } from "@/components/Footer";
 
