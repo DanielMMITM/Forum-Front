f6b726838f0fbfdaae53e032bf98b541f7c5202b
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-09 20:55:13 -0600

    Add - add login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,28 @@
+import { Box, Button, Grid, TextField } from "@mui/material";
+
+export const Login = () => {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      minHeight={"100vh"}
+    >
+      <h1 className="heading-primary">Login</h1>
+      <Box
+        display={"flex"}
+        alignContent={"center"}
+        flexDirection={"column"}
+        component={"form"}
+        rowGap={"2rem"}
+        minWidth={"30%"}
+        className="login-form"
+      >
+        <TextField label="Username" variant="filled" />
+        <TextField label="Password" variant="filled" />
+        <Button variant="contained">Login</Button>\
+      </Box>
+    </Box>
+  );
+};
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PrivateRouter from "./PrivateRouter";
 import { Home } from "@/pages/Home";
+import { Login } from "@/pages/Login";
 import { NavBar } from "@/components/NavBar";
 import { Footer } from "@/components/Footer";
 
@@ -10,7 +11,7 @@ export const AppRouter = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<h1>Login</h1>}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route element={<PrivateRouter />}>
           <Route path="/account"></Route>
           <Route path="/posts"></Route>
