57012b8ea33eb0bc995221cdda40a1b92e77b25a
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-14 23:02:46 -0600

    Add - add login request

---
+++ src/api/Auth/auth.ts
@@ -0,0 +1,6 @@
+import { axiosClient } from "@/config/axios";
+
+export const loginReq = async (body: any): Promise<any> => {
+  const { data } = await axiosClient.post("/auth/login", body);
+  return data;
+};
