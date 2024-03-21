3a22a8a6486fa38f36afe8bd8fe3a403e55ebb34
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-20 23:13:43 -0600

    Add - add login page and feature to login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,28 @@
-import { Box, Button, TextField } from "@mui/material";
+import { useLogin } from "@/hooks/Auth/useLogin";
+import { Box, Button, FormHelperText, TextField } from "@mui/material";
+import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: "all" });
+
+  const { login, isPending } = useLogin();
+
+  const onSubmit: SubmitHandler<LoginForm> = (data) => {
+    console.log("submit: ", data);
+    const body: Record<string, string> = {
+      username: data.username,
+      password: data.password,
+    };
+    login(body);
+  };
+
+  const onError: SubmitErrorHandler<LoginForm> = (data) => {
+    console.log("error: ", data);
+  };
   return (
     <Box
       display={"flex"}
@@ -14,14 +36,36 @@ export const Login = () => {
         display={"flex"}
         alignContent={"center"}
         flexDirection={"column"}
-        component={"form"}
         rowGap={"2rem"}
         minWidth={"30%"}
         className="login-form"
+        component={"form"}
+        onSubmit={handleSubmit(onSubmit, onError)}
+        autoComplete="off"
       >
-        <TextField label="Username" variant="filled" />
-        <TextField label="Password" variant="filled" />
-        <Button variant="contained">Login</Button>\
+        <TextField
+          label="Username"
+          variant="filled"
+          type="text"
+          {...register("username", { required: "Username is required" })}
+          error={!!errors.username}
+        />
+        <FormHelperText sx={{ color: "red", fontSize: "1.3rem" }}>
+          {errors?.username ? errors?.username?.message : ""}
+        </FormHelperText>
+        <TextField
+          label="Password"
+          variant="filled"
+          type="text"
+          {...register("password", { required: "Password is required" })}
+          error={!!errors.password}
+        />
+        <FormHelperText sx={{ color: "red", fontSize: "1.3rem" }}>
+          {errors?.password ? errors?.password?.message : ""}
+        </FormHelperText>
+        <Button variant="contained" type="submit" disabled={isPending}>
+          Login
+        </Button>
       </Box>
     </Box>
   );
