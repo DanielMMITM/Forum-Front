5d01a122d2c819796c5efb1675d84517c651ec7f
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-20 23:12:20 -0600

    Add - add button to add new posts

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,5 +1,5 @@
 import { CardPost } from "@/components/CardPost";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 export const Posts = () => {
   return (
@@ -9,8 +9,19 @@ export const Posts = () => {
       minHeight={"100vh"}
       marginTop={"calc(100vh - (100vh - 76px))"}
       padding={"6rem"}
+      className="posts"
     >
-      <h2 className="heading-secondary heading-secondary--posts">Posts</h2>
+      <Box display={"flex"} className="posts__header">
+        <Box display={"flex"} mr={"auto"}>
+          <h2 className="heading-secondary heading-secondary--posts">Posts</h2>
+        </Box>
+        <Button
+          variant="contained"
+          sx={{ alignSelf: "center", fontSize: "1.3rem" }}
+        >
+          New Post
+        </Button>
+      </Box>
       <CardPost />
       <CardPost />
       <CardPost />
