0d9fcc1b076f8d2c2dfeb40a82856b2d28ba1d26
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-25 13:18:35 -0600

    Update - Update style on error message component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, FormHelperText, TextField } from "@mui/material";
 import { useLoginForm } from "@/utils/hooks/Auth/Login/useLoginForm";
+import { capitalizeString } from "@/utils/helpers/capitalizeString";
+import { PASSWORD_FIELD, USERNAME_FIELD } from "@/utils/constants/Auth/Login";
 
 export const Login = () => {
   const {
@@ -29,31 +31,31 @@ export const Login = () => {
         flexDirection={"column"}
         rowGap={"2rem"}
         minWidth={"30%"}
-        className="form-container "
+        className="form-container"
         component={"form"}
         onSubmit={handleSubmit(onSubmit, onError)}
         autoComplete="off"
       >
         <TextField
-          label="Username"
+          label={capitalizeString(USERNAME_FIELD)}
           variant="filled"
           type="text"
           ref={usernameReference}
           {...usernameProps}
           error={!!errors.username}
         />
-        <FormHelperText sx={{ color: "red", fontSize: "1.3rem" }}>
+        <FormHelperText className="form-container__error">
           {errors?.username ? errors?.username?.message : ""}
         </FormHelperText>
         <TextField
-          label="Password"
+          label={capitalizeString(PASSWORD_FIELD)}
           variant="filled"
           type="text"
           ref={passwordReference}
           {...passwordProps}
           error={!!errors.password}
         />
-        <FormHelperText sx={{ color: "red", fontSize: "1.3rem" }}>
+        <FormHelperText className="form-container__error">
           {errors?.password ? errors?.password?.message : ""}
         </FormHelperText>
         <Button variant="contained" type="submit" disabled={isPending}>
