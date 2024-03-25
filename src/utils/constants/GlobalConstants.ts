8e67b5c8f16365af858aeb3c6d0ac7e1e7e62ebb
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-25 14:31:06 -0600

    Add - add empty constant

--- src/utils/constants/GlobalConstants.ts
+++ src/utils/constants/GlobalConstants.ts
@@ -1,3 +1,5 @@
 export const REQUIRED_FIELD_ERROR = " is required";
 export const MAX_LENGTH_ERROR = " must have less than ";
 export const CHARACTERS_WORD_ERROR = " characters";
+
+export const EMPTY = "";
