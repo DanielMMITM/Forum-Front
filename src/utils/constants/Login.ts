83caa84277edb9054cfe1cc90d449fb9bca8ec9c
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-25 12:49:27 -0600

    Add - add constants file to set messages and variables

---
+++ src/utils/constants/Login.ts
@@ -0,0 +1,7 @@
+export const USERNAME_FIELD = "username";
+export const PASSWORD_FIELD = "password";
+export const MAX_LENGTH = 40;
+
+export const REQUIRED_FIELD_ERROR = " is required";
+export const MAX_LENGTH_ERROR = " must have less than ";
+export const CHARACTERS_WORD_ERROR = " characters";
