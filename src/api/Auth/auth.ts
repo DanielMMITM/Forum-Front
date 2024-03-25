071edc6e94635dfb39c541b951f54a4fb373be14
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-24 20:21:44 -0600

    Update - update auth request name and redirect on success

--- src/api/Auth/auth.ts
+++ src/api/Auth/auth.ts
@@ -1,8 +1,9 @@
 import { axiosClient } from "@/config/axios";
 
-export const loginReq = async (
+export const loginRequest = async (
   body: Record<string, string>
 ): Promise<LoginResponse> => {
   const { data } = await axiosClient.post("/auth/login", body);
+  console.log(data);
   return data;
 };
--- src/utils/hooks/Auth/useLogin.ts
+++ src/utils/hooks/Auth/useLogin.ts
@@ -1,14 +1,14 @@
-import { loginReq } from "@/api/Auth/auth";
+import { loginRequest } from "@/api/Auth/auth";
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
   const navigate = useNavigate();
   const { mutate: login, isPending } = useMutation({
-    mutationFn: (body: Record<string, string>) => loginReq(body),
+    mutationFn: (body: Record<string, string>) => loginRequest(body),
     onSuccess: (data) => {
       localStorage.setItem("token", data.token);
-      navigate("/home");
+      navigate("/");
     },
     onError: (error) => {
       alert("Invalid credentials: " + error.message);
