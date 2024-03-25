47fc1e6d222aed8f7dc9fcb270308d4eabf5c05d
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-25 14:29:18 -0600

    Add - add placeholder constants

--- src/utils/constants/Posts/Posts.ts
+++ src/utils/constants/Posts/Posts.ts
@@ -2,5 +2,8 @@ export const TITLE_FIELD = "title";
 export const TEXT_FIELD = "text";
 export const COURSE_FIELD = "course";
 
+export const TITLE_PLACEHOLDER = "Write the title of the post...";
+export const TEXT_PLACEHOLDER = "Describe what is your post about...";
+
 export const TITLE_MAX_LENGTH = 50;
 export const TEXT_MAX_LENGTH = 250;
