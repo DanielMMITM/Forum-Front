194c9cbd1ce0679e20f2aa25a312ea5d1a00be06
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-26 13:26:50 -0600

    Add - implement show posts hook and add spinner and pagination component

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,9 +1,31 @@
 import { CardPost } from "@/components/CardPost";
-import { Box, Button } from "@mui/material";
+import { useShowPosts } from "@/utils/hooks/Posts/useShowPosts";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Pagination,
+  PaginationItem,
+} from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { useNavigate } from "react-router-dom";
 
 export const Posts = () => {
   const navigate = useNavigate();
+  const { posts, pageable, totalPages, isLoading } = useShowPosts();
+
+  if (isLoading && !posts)
+    return (
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        minHeight={"100vh"}
+      >
+        <CircularProgress />
+      </Box>
+    );
   return (
     <Box
       display={"flex"}
@@ -25,9 +47,31 @@ export const Posts = () => {
           New Post
         </Button>
       </Box>
-      <CardPost />
-      <CardPost />
-      <CardPost />
+      {!isLoading && !posts ? (
+        <Box display={"flex"} mt={"20rem"} justifyContent={"center"}>
+          <h1 className="heading-primary">
+            Whoops!... There are no posts to show
+          </h1>
+        </Box>
+      ) : (
+        <>
+          <CardPost />
+          <Box display={"flex"} alignSelf={"end"} mt={"4rem"}>
+            <Pagination
+              size="large"
+              renderItem={(item) => (
+                <PaginationItem
+                  slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+                  {...item}
+                />
+              )}
+              count={totalPages}
+              page={pageable.pageNumber + 1}
+              color="secondary"
+            />
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
