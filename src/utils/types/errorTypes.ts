6d5fe67136a10ffc554663c08d6ccc08f1a24540
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-30 22:56:46 -0600

    Add - add error types

---
+++ src/utils/types/errorTypes.ts
@@ -0,0 +1,14 @@
+interface CustomError {
+  code: number;
+  status: string;
+  message: string | null;
+  debugMessage: string | null;
+  stack: SubError[] | null;
+}
+
+interface SubError {
+  object: string;
+  field: string;
+  rejectedValue: Object;
+  message: string;
+}
