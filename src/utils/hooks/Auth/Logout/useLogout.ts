874b6c75760ff0371170af9fdde8ccaa0fda1258
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-31 01:17:38 -0600

    Update - update logout hook

--- src/utils/hooks/Auth/Logout/useLogout.ts
+++ src/utils/hooks/Auth/Logout/useLogout.ts
@@ -1,10 +1,6 @@
-import { useNavigate } from "react-router-dom";
-
 export const useLogout = () => {
-  const navigate = useNavigate();
   function logout() {
     localStorage.removeItem("token");
-    navigate("/");
   }
 
   return { logout };
