6329b34c6fe7efe95871555c6253983615c6b005
Edgarm <edgar.martinez@canteradigital.mx>
2024-03-31 01:19:12 -0600

    Update - update error handler for requests

--- src/utils/helpers/handleErrorsResponse.ts
+++ src/utils/helpers/handleErrorsResponse.ts
@@ -1,25 +1,26 @@
 import toast from "react-hot-toast";
-import { useLogout } from "@/utils/hooks/Auth/Logout/useLogout";
+import { HttpErrors } from "@/utils/enum/HttpErrors";
+import { useLogout } from "../hooks/Auth/Logout/useLogout";
 
-export function handleErrorsResponse(error: any) {
+export const handleErrorsResponse = (error: any) => {
   const { logout } = useLogout();
   toast.dismiss();
 
-  console.log(error.response.data);
-
-  switch (error.response.data.code) {
+  switch (error.response?.data?.code) {
     case HttpErrors.UNAUTHORIZED: {
-      toast.error(error.response.data.message);
-      logout;
+      toast.error(error.response?.data?.message, {
+        className: "toast",
+      });
+      logout();
       break;
     }
     default: {
       toast.error(
-        error.response.data.message
-          ? error.response.data.message
-          : error.response.data.debugMessage
+        error.response?.data?.message
+          ? error.response?.data?.message
+          : error.response?.data?.debugMessage
       );
       break;
     }
   }
-}
+};
