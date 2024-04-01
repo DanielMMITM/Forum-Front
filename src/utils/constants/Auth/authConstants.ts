d7d37f0fcfd4b09ac13336fcc7f0d60e83e91213
Edgarm <edgar.martinez@canteradigital.mx>
2024-04-01 16:24:13 -0600

    Update - rename constant label

--- src/utils/constants/Auth/authConstants.ts
+++ src/utils/constants/Auth/authConstants.ts
@@ -4,7 +4,7 @@ export const MAX_LENGTH = 30;
 
 export const EMAIL_FIELD = "email";
 export const MAX_LENGTH_EMAIL = 60;
-export const CONFIRM_PASSWORD_LABEL = "Confirmar contraseña";
+export const CONFIRM_PASSWORD_LABEL = "Confirm password";
 export const CONFIRM_PASSWORD_FIELD = "confirmPassword";
 
 export const CONFIRM_PASSWORD_ERROR = `Passwords don't match`;
