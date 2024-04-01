911eff184bfbee586039eb9f2f2d7d1f5ec74591
Edgarm <edgar.martinez@canteradigital.mx>
2024-04-01 14:51:20 -0600

    Update - refactor signup and login constants into one single file

--- src/utils/constants/Auth/Login.ts
+++
@@ -1,3 +0,0 @@
-export const USERNAME_FIELD = "username";
-export const PASSWORD_FIELD = "password";
-export const MAX_LENGTH = 40;
--- src/utils/constants/Auth/authConstants.ts
+++ src/utils/constants/Auth/authConstants.ts
@@ -1,8 +1,9 @@
 export const USERNAME_FIELD = "username";
-export const EMAIL_FIELD = "email";
 export const PASSWORD_FIELD = "password";
-export const CONFIRM_PASSWORD_FIELD = "confirmPassword";
 export const MAX_LENGTH = 30;
+
+export const EMAIL_FIELD = "email";
 export const MAX_LENGTH_EMAIL = 60;
+export const CONFIRM_PASSWORD_FIELD = "confirmPassword";
 
 export const CONFIRM_PASSWORD_ERROR = `Passwords don't match`;
