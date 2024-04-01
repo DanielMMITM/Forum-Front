d9a6b97e64a2fe7f425b9275fbf0feb556a36fec
Edgarm <edgar.martinez@canteradigital.mx>
2024-04-01 16:03:26 -0600

    Add - add confirm password label constant

--- src/utils/constants/Auth/authConstants.ts
+++ src/utils/constants/Auth/authConstants.ts
@@ -4,6 +4,7 @@ export const MAX_LENGTH = 30;
 
 export const EMAIL_FIELD = "email";
 export const MAX_LENGTH_EMAIL = 60;
+export const CONFIRM_PASSWORD_LABEL = "Confirmar contraseña";
 export const CONFIRM_PASSWORD_FIELD = "confirmPassword";
 
 export const CONFIRM_PASSWORD_ERROR = `Passwords don't match`;
