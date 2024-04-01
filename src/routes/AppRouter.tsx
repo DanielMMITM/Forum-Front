b4a87a8fabcb528a232b02524423af30f31726c2
Edgarm <edgar.martinez@canteradigital.mx>
2024-04-01 12:48:04 -0600

    Add - add register route and update profile path

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,6 +6,7 @@ import { NavBar } from "@/components/NavBar";
 import { Footer } from "@/components/Footer";
 import { Posts } from "@/pages/Posts/Posts";
 import { NewPost } from "@/pages/Posts/NewPost";
+import { Register } from "@/pages/Register";
 
 export const AppRouter = () => {
   return (
@@ -14,8 +15,9 @@ export const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
         <Route element={<PrivateRouter />}>
-          <Route path="/account"></Route>
+          <Route path="/profile"></Route>
           <Route path="/posts" element={<Posts />}></Route>
           <Route path="/new-post" element={<NewPost />}></Route>
         </Route>
