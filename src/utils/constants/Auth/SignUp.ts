91e143417a1719041d791da036e9b805fe54e9b7
Edgarm <edgar.martinez@canteradigital.mx>
2024-04-01 14:45:44 -0600

    Add - add constans for signup form

---
+++ src/utils/constants/Auth/SignUp.ts
@@ -0,0 +1,8 @@
+export const USERNAME_FIELD = "username";
+export const EMAIL_FIELD = "email";
+export const PASSWORD_FIELD = "password";
+export const CONFIRM_PASSWORD_FIELD = "confirmPassword";
+export const MAX_LENGTH = 30;
+export const MAX_LENGTH_EMAIL = 60;
+
+export const CONFIRM_PASSWORD_ERROR = `Passwords don't match`;
