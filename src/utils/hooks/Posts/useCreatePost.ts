4cbfaac7fbf78ef8fe9a639b0a390110605bbc1c
Edgarm <edgar.martinez@canteradigital.mx>
2024-05-02 15:18:22 -0600

    Update - update navigate to the post on successful record created

--- src/utils/hooks/Posts/useCreatePost.ts
+++ src/utils/hooks/Posts/useCreatePost.ts
@@ -1,20 +1,24 @@
 import { createPostRequest } from "@/api/Posts/posts";
 import { handleErrorsResponse } from "@/utils/helpers/handleErrorsResponse";
+import { CustomAxiosError } from "@/utils/types/errorTypes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const useCreatePost = () => {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
 
   const { mutate: createPost, isPending } = useMutation({
     mutationFn: (body: Record<string, string | number>) =>
       createPostRequest(body),
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Post created successfully!", { className: "toast" });
       queryClient.invalidateQueries({ queryKey: ["posts"] });
+      navigate(`/posts/${data.id}`);
     },
-    onError: (error: any) => {
-      handleErrorsResponse(error);
+    onError: (error: CustomAxiosError) => {
+      handleErrorsResponse(error.response.data);
     },
   });
   return { createPost, isPending };
