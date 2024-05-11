bd993c4a8443d8b9ab3784ecadbe86cfcf3205a3
Edgarm <edgar.martinez@canteradigital.mx>
2024-05-11 06:23:19 -0600

    Add - add devtools to debug the state

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,15 +1,18 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 interface UserStore {
   id: number | null;
   setId: (id: number | null) => void;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
-  id: null,
-  setId: (id: number | null): void => {
-    set(() => ({
-      id: id,
-    }));
-  },
-}));
+export const useUserStore = create<UserStore>()(
+  devtools((set) => ({
+    id: null,
+    setId: (id: number | null): void => {
+      set(() => ({
+        id: id,
+      }));
+    },
+  }))
+);
