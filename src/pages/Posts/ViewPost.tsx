c0b7922a5b4969f47c8bdce026df24be9c2083e4
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-10 15:21:58 -0600

    Add - add back and edit button, also the course of the post

--- src/pages/Posts/ViewPost.tsx
+++ src/pages/Posts/ViewPost.tsx
@@ -1,10 +1,13 @@
 import { CardAnswers } from "@/components/CardAnswers";
 import { Box, Button, FormHelperText, Grid } from "@mui/material";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import profilePic from "@/assets/images/profile.webp";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export const ViewPost = () => {
-  const { state: post } = useLocation();
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const post = state as PostResponse;
 
   return (
     <Box
@@ -15,6 +18,16 @@ export const ViewPost = () => {
       className="posts"
       minHeight={"80vh"}
     >
+      <Box display={"flex"} fontSize={"1.5rem"}>
+        <Button
+          variant="text"
+          color="info"
+          sx={{ fontSize: "inherit" }}
+          onClick={() => navigate(-1)}
+        >
+          <ArrowBackIcon fontSize={"inherit"} /> Back
+        </Button>
+      </Box>
       <Box>
         <h1 className=" heading-primary">{post.title}</h1>
         <Box
@@ -32,32 +45,38 @@ export const ViewPost = () => {
             <h2>{post.userCreator.username}</h2>
           </Box>
           <Box>
-            <h2>{post.statusPost}</h2>
+            <h2>Curso: {post.courseId}</h2>
+            <h3>{post.statusPost}</h3>
           </Box>
         </Box>
-      </Box>
-      <p className="text">{post.text}</p>
-      <Grid
-        container
-        spacing={2}
-        className="form-container"
-        justifyContent={"end"}
-      >
-        <Grid item xs={12} sx={{ fontSize: "1.5rem" }}>
-          <Box display={"flex"} flexDirection={"column"} className="posts">
-            <label id="--custom-label">Add a comment</label>
-            <textarea id="--text-area" />
-          </Box>
-          <FormHelperText className="form-container__error form-container__error--posts"></FormHelperText>
-        </Grid>
-        <Grid item xs={12} md={2}>
-          <Button variant="contained" fullWidth type="submit">
-            Create
+        <p className="text">{post.text}</p>
+        <Box display={"flex"} justifyContent={"end"}>
+          <Button variant="contained" type="submit" sx={{ fontSize: "1.5rem" }}>
+            Edit Post
           </Button>
-        </Grid>
-      </Grid>
+        </Box>
+      </Box>
 
-      <Box mt={"4rem"} borderTop={"2px solid gray"}>
+      <Box borderTop={"2px solid gray"} mt={"2rem"}>
+        <Grid
+          container
+          spacing={2}
+          className="form-container"
+          justifyContent={"end"}
+        >
+          <Grid item xs={12} sx={{ fontSize: "1.5rem" }} mt={"2rem"}>
+            <Box display={"flex"} flexDirection={"column"} className="posts">
+              <label id="--custom-label">Add a comment</label>
+              <textarea id="--text-area" />
+            </Box>
+            <FormHelperText className="form-container__error form-container__error--posts"></FormHelperText>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Button variant="contained" fullWidth type="submit">
+              Create
+            </Button>
+          </Grid>
+        </Grid>
         <Box mt={"2rem"}>
           {post.answers.map((answer: any) => (
             <CardAnswers
