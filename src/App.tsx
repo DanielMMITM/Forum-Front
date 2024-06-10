510d0929c66fb10d59de9c6890ff72d9e432089a
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-10 14:43:56 -0600

    Add - add custom mui theme provider to add and modify colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppRouter } from "@/routes/AppRouter";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import { CustomMUITheme } from "./context/CustomMUITheme";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <AppRouter />
+      <CustomMUITheme>
+        <AppRouter />
+      </CustomMUITheme>
       <Toaster />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
---
+++ src/context/CustomMUITheme.tsx
@@ -0,0 +1,49 @@
+import { ThemeProvider, createTheme } from "@mui/material";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    tertiary: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    tertiary?: PaletteOptions["primary"];
+  }
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    tertiary: true;
+  }
+}
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#038ad8",
+      light: "#2998e1",
+      dark: "#003459",
+    },
+    secondary: {
+      main: "#efbdeb",
+      dark: "#b68cb8",
+    },
+    info: {
+      main: "#616161",
+    },
+    tertiary: {
+      main: "#c2d076",
+      light: "#E9DB5D",
+      dark: "#A29415",
+      contrastText: "#242105",
+    },
+  },
+});
+
+export const CustomMUITheme = ({ children }: Props) => {
+  return <ThemeProvider theme={customTheme}>{children}</ThemeProvider>;
+};
