cf9332d5cf82aeee29714cd2dbecb649b3270363
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-13 17:26:14 -0600

    Add - add status indicator component for viewpost and post card

---
+++ src/components/StatusIndicator.tsx
@@ -0,0 +1,21 @@
+import { formatStatus } from "@/utils/helpers/formatStatus";
+import { handleStatusColor } from "@/utils/helpers/handleStatusColor";
+import { Box } from "@mui/material";
+
+interface Props {
+  status: string;
+}
+export const StatusIndicator = ({ status }: Props) => {
+  const { color, border } = handleStatusColor(status);
+  return (
+    <Box
+      display={"flex"}
+      padding={0.7}
+      borderRadius={3}
+      border={`2px solid ${border}`}
+      sx={{ backgroundColor: color }}
+    >
+      <h3>{formatStatus(status)}</h3>
+    </Box>
+  );
+};
