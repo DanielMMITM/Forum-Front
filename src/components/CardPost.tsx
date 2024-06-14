35516efee57f6ee8d57753c76dbc1fbbf36dfc94
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-13 18:25:26 -0600

    Update - post card style to add the status and link to show the post

--- src/components/CardPost.tsx
+++ src/components/CardPost.tsx
@@ -2,6 +2,8 @@ import { Box } from "@mui/material";
 import profilePic from "@/assets/images/profile.webp";
 import { Link } from "react-router-dom";
 import { PostResponse } from "@/utils/types/postTypes";
+import { StatusIndicator } from "./StatusIndicator";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 interface CardProps {
   data: PostResponse;
@@ -13,6 +15,7 @@ export const CardPost = ({ data }: CardProps) => {
     title,
     text,
     userCreator: { username },
+    statusPost,
   } = data;
   return (
     <Box
@@ -42,14 +45,35 @@ export const CardPost = ({ data }: CardProps) => {
         flexDirection={"column"}
         alignSelf={"start"}
         marginInlineStart={"2rem"}
-        marginInlineEnd={"6rem"}
+        marginInlineEnd={"1.5rem"}
+        flexGrow={1}
       >
-        <Link to={`/posts/${id}`} replace={true} state={data} className="posts">
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          flexBasis={"90%"}
+        >
           <h2 className=" heading-secondary heading-secondary--card-post__title">
             {title}
           </h2>
-        </Link>
+          <StatusIndicator status={statusPost} />
+        </Box>
         <p className="text--card-post">{text}</p>
+        <Box
+          display={"flex"}
+          justifyContent={"flex-end"}
+          m={"3rem 3rem 1rem 0"}
+        >
+          <Link
+            to={`/posts/${id}`}
+            replace={true}
+            state={data}
+            className="posts--show-btn"
+          >
+            Show <ArrowForwardIcon />
+          </Link>
+        </Box>
       </Box>
     </Box>
   );
