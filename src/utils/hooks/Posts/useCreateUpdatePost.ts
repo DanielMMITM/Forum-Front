d54c5af5c3790ee1d85c8b9c3d2a19c61f8d3800
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-14 14:31:22 -0600

    Update - rename file and added logic to handle if is updating or creating

--- src/utils/hooks/Posts/useCreateUpdatePost.ts
+++ src/utils/hooks/Posts/useCreateUpdatePost.ts
@@ -1,19 +1,25 @@
-import { createPostRequest } from "@/api/Posts/posts";
+import { createPostRequest, updatePostRequest } from "@/api/Posts/posts";
 import { handleErrorsResponse } from "@/utils/helpers/handleErrorsResponse";
+import { ActionTypes } from "@/utils/types/commonTypes";
 import { CustomAxiosError } from "@/utils/types/errorTypes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
-export const useCreatePost = () => {
+export const useCreateUpdatePost = (action: ActionTypes) => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
-  const { mutate: createPost, isPending } = useMutation({
+  const { mutate: createUpdatePost, isPending } = useMutation({
     mutationFn: (body: Record<string, string | number>) =>
-      createPostRequest(body),
+      action === "Create" ? createPostRequest(body) : updatePostRequest(body),
     onSuccess: (data) => {
-      toast.success("Post created successfully!", { className: "toast" });
+      toast.success(
+        `Post ${action === "Create" ? "created" : "updated"} successfully!`,
+        {
+          className: "toast",
+        }
+      );
       queryClient.invalidateQueries({ queryKey: ["posts"] });
       navigate(`/posts/${data.id}`, { replace: true, state: data });
     },
@@ -21,5 +27,5 @@ export const useCreatePost = () => {
       handleErrorsResponse(error.response.data);
     },
   });
-  return { createPost, isPending };
+  return { createUpdatePost, isPending };
 };
