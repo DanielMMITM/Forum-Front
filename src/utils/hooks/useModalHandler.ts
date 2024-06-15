646fa961fc1f2c7a1d1cd8a1992bea7be495d3a6
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-14 19:45:26 -0600

    Add - add utils to handle modal visibility

---
+++ src/utils/hooks/useModalHandler.ts
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export const useModalHandler = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
+  return { open, handleOpenModal, handleCloseModal };
+};
