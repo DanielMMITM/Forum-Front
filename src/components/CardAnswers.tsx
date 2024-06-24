2ee016188c311b354a1116dfeb7d958f6ff364d0
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-23 20:36:53 -0600

    Add - add style to the response card

--- src/components/CardAnswers.tsx
+++ src/components/CardAnswers.tsx
@@ -9,26 +9,31 @@ interface CardAnswerProps {
 
 export const CardAnswers = ({ id, text, solution }: CardAnswerProps) => {
   return (
-    <Box display={"flex"} flexDirection={"row"} key={id} mt={"2rem"}>
+    <Box
+      display={"flex"}
+      flexDirection={"row"}
+      key={id}
+      mt={"2rem"}
+      className="card-response"
+      alignItems={'start'}
+    >
       <Box
         display={"flex"}
         flexDirection={"column"}
         flexBasis={"20%"}
-        alignItems={"center"}
+        alignSelf={"center"}
       >
-        <Box display={"flex"} className="card-post__img">
+        <Box display={"flex"} justifyContent={"center"}>
           <img
             src={profilePic}
             alt="Profile Picture"
-            className="card-post__picture"
+            className="card-response__picture"
           />
         </Box>
-        <Box display={"flex"}>
-          <h2 className="">Leonel Messi</h2>
-        </Box>
+        <h2 className="heading-secondary--card-response__name">Leonel Messi</h2>
       </Box>
-      <Box display={"flex"} flexDirection={"column"} flexBasis={"80%"}>
-        <p>{text}</p>
+      <Box display={"flex"} flexBasis={"80%"} mt={-1}>
+        <p className="text--card-response">{text}</p>
       </Box>
     </Box>
   );
