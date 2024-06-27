58297e2957d91d63baa5e18fa3d6da94f9c0065c
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-27 16:26:15 -0600

    Add - add constants and form logic to add response

---
+++ src/utils/constants/Response/responseConstants.ts
@@ -0,0 +1,5 @@
+export const TEXT_FIELD = 'text';
+
+export const TEXT_PLACEHOLDER = 'Help sharing your knowledge';
+
+export const TEXT_MAX_LENGTH = 700;
---
+++ src/utils/hooks/Response/useAddResponseForm.ts
@@ -0,0 +1,66 @@
+import {
+  CHARACTERS_WORD_ERROR,
+  MAX_LENGTH_ERROR,
+  REQUIRED_FIELD_ERROR,
+} from '@/utils/constants/GlobalConstants';
+import { capitalizeString } from '@/utils/helpers/capitalizeString';
+import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
+import { useUserStore } from '@/store/userStore';
+import { ResponseForm } from '@/utils/types/responseTypes';
+import { useAddResponse } from './useAddResponse';
+import { TEXT_FIELD, TEXT_MAX_LENGTH } from '@/utils/constants/Response/responseConstants';
+
+export const useAddResponseForm = (postId: number) => {
+  const { id } = useUserStore();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<ResponseForm>({
+    defaultValues: {
+      text: '',
+    },
+  });
+
+  const { addResponse, isPending } = useAddResponse();
+
+  function updateFields(): void {
+    reset();
+  }
+
+  const { ref: textReference, ...textProps } = register(TEXT_FIELD, {
+    required: `${capitalizeString(TEXT_FIELD)}${REQUIRED_FIELD_ERROR}`,
+    maxLength: {
+      value: TEXT_MAX_LENGTH,
+      message: `${capitalizeString(
+        TEXT_FIELD
+      )}${MAX_LENGTH_ERROR}${TEXT_MAX_LENGTH}${CHARACTERS_WORD_ERROR}`,
+    },
+  });
+
+  const onSubmit: SubmitHandler<ResponseForm> = (data) => {
+    console.log('submit: ', data);
+    const { text } = data;
+    const body: Record<string, string | number> = {
+      title: text,
+      postId: postId,
+      userId: Number(id),
+    };
+    addResponse(body, { onSuccess: updateFields });
+  };
+
+  const onError: SubmitErrorHandler<ResponseForm> = (data) => {
+    console.log('error: ', data);
+  };
+
+  return {
+    textReference,
+    textProps,
+    handleSubmit,
+    onSubmit,
+    onError,
+    errors,
+    isPending,
+  };
+};
