b9ad035470acf966b6bffb3602259edfc7565c43
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-27 13:40:34 -0600

    Update - update classes and format document

--- src/pages/Posts/CreateUpdatePost.tsx
+++ src/pages/Posts/CreateUpdatePost.tsx
@@ -1,14 +1,11 @@
-import { Spinner } from "@/components/Spinner";
-import { EMPTY } from "@/utils/constants/GlobalConstants";
-import {
-  TEXT_PLACEHOLDER,
-  TITLE_PLACEHOLDER,
-} from "@/utils/constants/Posts/PostsConstants";
-import { useCourses } from "@/utils/hooks/Posts/useCourses";
-import { usePostsForm } from "@/utils/hooks/Posts/usePostsForm";
-import { ActionTypes } from "@/utils/types/commonTypes";
-import { Course } from "@/utils/types/courseTypes";
-import { PostResponse } from "@/utils/types/postTypes";
+import { Spinner } from '@/components/Spinner';
+import { EMPTY } from '@/utils/constants/GlobalConstants';
+import { TEXT_PLACEHOLDER, TITLE_PLACEHOLDER } from '@/utils/constants/Posts/PostsConstants';
+import { useCourses } from '@/utils/hooks/Posts/useCourses';
+import { usePostsForm } from '@/utils/hooks/Posts/usePostsForm';
+import { ActionTypes } from '@/utils/types/commonTypes';
+import { Course } from '@/utils/types/courseTypes';
+import { PostResponse } from '@/utils/types/postTypes';
 import {
   Box,
   Button,
@@ -18,9 +15,9 @@ import {
   MenuItem,
   Select,
   TextField,
-} from "@mui/material";
-import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+} from '@mui/material';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const CreateUpdatePost = () => {
   const { state } = useLocation();
@@ -45,8 +42,8 @@ export const CreateUpdatePost = () => {
   } = usePostsForm({ post, action });
 
   useEffect(() => {
-    if ((!post && action === "Update") || !action) {
-      navigate("/");
+    if ((!post && action === 'Update') || !action) {
+      navigate('/');
     }
   }, []);
 
@@ -56,33 +53,29 @@ export const CreateUpdatePost = () => {
 
   return (
     <Box
-      display={"flex"}
-      flexDirection={"column"}
-      marginTop={"calc(100vh - (100vh - 50px))"}
-      padding={"6rem"}
+      display={'flex'}
+      flexDirection={'column'}
+      marginTop={'calc(100vh - (100vh - 50px))'}
+      padding={'6rem'}
       className="posts"
     >
-      <Box display={"flex"} className="posts__header">
-        <Box display={"flex"} mr={"auto"}>
-          <h2 className="heading-secondary heading-secondary--posts">
-            {action} post
-          </h2>
+      <Box display={'flex'} className="posts__header">
+        <Box display={'flex'} mr={'auto'}>
+          <h2 className="heading-secondary heading-secondary--posts">{action} post</h2>
         </Box>
       </Box>
       <Container>
         <Grid
           container
-          marginTop={"3rem"}
-          gridAutoFlow={"column"}
+          marginTop={'3rem'}
+          gridAutoFlow={'column'}
           rowGap={2}
-          component={"form"}
+          component={'form'}
           className="form-container"
           onSubmit={handleSubmit(onSubmit, onError)}
         >
           <Grid item xs={12}>
-            <label id="--custom-label" className="heading-secondary">
-              Title
-            </label>
+            <label className="heading-secondary">Title</label>
             <TextField
               type="text"
               placeholder={TITLE_PLACEHOLDER}
@@ -97,30 +90,24 @@ export const CreateUpdatePost = () => {
             </FormHelperText>
           </Grid>
           <Grid item xs={12} md={8}>
-            <label id="--custom-label" className="heading-secondary">
-              Course
-            </label>
+            <label className="heading-secondary">Course</label>
             <Select
               fullWidth
               labelId="demo-simple-select-standard-label"
               id="demo-simple-select-standard"
               value={course}
               onChange={handleChange}
-              sx={{ fontSize: "1.6rem" }}
+              sx={{ fontSize: '1.6rem' }}
               size="small"
               ref={courseReference}
               inputProps={{ ...courseProps }}
               disabled={isPending}
             >
-              <MenuItem value={0} sx={{ fontSize: "1.6rem" }} disabled>
+              <MenuItem value={0} sx={{ fontSize: '1.6rem' }} disabled>
                 <em>Select a course</em>
               </MenuItem>
               {courses?.map((course: Course) => (
-                <MenuItem
-                  key={course.id}
-                  value={course.id}
-                  sx={{ fontSize: "1.6rem" }}
-                >
+                <MenuItem key={course.id} value={course.id} sx={{ fontSize: '1.6rem' }}>
                   {course.name}
                 </MenuItem>
               ))}
@@ -129,12 +116,10 @@ export const CreateUpdatePost = () => {
               {errors?.course ? errors?.course?.message : EMPTY}
             </FormHelperText>
           </Grid>
-          <Grid item xs={12} sx={{ fontSize: "1.5rem" }}>
-            <label id="--custom-label" className="heading-secondary">
-              Text
-            </label>
+          <Grid item xs={12} sx={{ fontSize: '1.5rem' }} display={'flex'} flexDirection={'column'}>
+            <label className="heading-secondary">Text</label>
             <textarea
-              id="--text-area"
+              className="form-container__text-area--post"
               placeholder={TEXT_PLACEHOLDER}
               ref={textReference}
               {...textProps}
@@ -144,14 +129,14 @@ export const CreateUpdatePost = () => {
               {errors?.text ? errors?.text?.message : EMPTY}
             </FormHelperText>
           </Grid>
-          <Grid container item xs={12} columnGap={4} justifyContent={"end"}>
+          <Grid container item xs={12} columnGap={4} justifyContent={'end'}>
             <Grid item xs={12} md={2} order={{ xs: 2, md: 1 }}>
               <Button
                 variant="contained"
                 color="error"
                 fullWidth
                 onClick={() =>
-                  action === "Create"
+                  action === 'Create'
                     ? navigate(-1)
                     : navigate(`/posts/${post.id}`, {
                         replace: true,
@@ -163,19 +148,8 @@ export const CreateUpdatePost = () => {
                 Back
               </Button>
             </Grid>
-            <Grid
-              item
-              xs={12}
-              md={2}
-              order={{ xs: 1, md: 2 }}
-              marginBottom={{ xs: "2rem", md: 0 }}
-            >
-              <Button
-                variant="contained"
-                fullWidth
-                type="submit"
-                disabled={isPending}
-              >
+            <Grid item xs={12} md={2} order={{ xs: 1, md: 2 }} marginBottom={{ xs: '2rem', md: 0 }}>
+              <Button variant="contained" fullWidth type="submit" disabled={isPending}>
                 {action}
               </Button>
             </Grid>
