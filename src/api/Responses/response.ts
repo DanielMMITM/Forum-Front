87818a2f87b11951c52657d117490e06d7222f4e
Edgarm <edgar.martinez@canteradigital.mx>
2024-06-29 12:41:58 -0600

    Add - add request and hook to set the solution in a response

--- src/api/Responses/response.ts
+++ src/api/Responses/response.ts
@@ -7,3 +7,9 @@ export const AddResponseRequest = async (
   const { data } = await axiosClient.post('/responses', body);
   return data;
 };
+
+export const checkSolutionRequest = async (id: number): Promise<Response> => {
+  getToken();
+  const { data } = await axiosClient.put(`/responses/${id}`);
+  return data;
+};
---
+++ src/utils/hooks/Response/useCheckSolution.ts
@@ -0,0 +1,24 @@
+import { checkSolutionRequest } from '@/api/Responses/response';
+import { handleErrorsResponse } from '@/utils/helpers/handleErrorsResponse';
+import { CustomAxiosError } from '@/utils/types/errorTypes';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
+export const useCheckSolution = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: checkSolution, isPending: isPendingSolution } = useMutation({
+    mutationFn: (id: number) => checkSolutionRequest(id),
+    onSuccess: () => {
+      toast.success(`Solution checked successfully!`, {
+        className: 'toast',
+      });
+      queryClient.invalidateQueries({ queryKey: ['post'] });
+    },
+    onError: (error: CustomAxiosError) => {
+      handleErrorsResponse(error.response.data);
+    },
+  });
+
+  return { checkSolution, isPendingSolution };
+};
