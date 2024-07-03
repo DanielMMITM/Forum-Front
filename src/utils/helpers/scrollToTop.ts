1be3045df63ce6ffeb80fd9d25a3b7e5a9c38d44
Edgarm <edgar.martinez@canteradigital.mx>
2024-07-03 14:03:59 -0600

    Update - update scroll to top to execute on pagination param change

--- src/utils/helpers/scrollToTop.ts
+++ src/utils/helpers/scrollToTop.ts
@@ -1,7 +1,9 @@
 import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export function scrollToTop() {
+  const [_params, setParams] = useSearchParams();
   useMemo(() => {
     window.scrollTo({ top: 0 });
-  }, []);
+  }, [setParams, []]);
 }
