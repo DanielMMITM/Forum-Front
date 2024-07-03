682fa20b9969c5db44bf041a4b45f23ad7024457
Edgarm <edgar.martinez@canteradigital.mx>
2024-07-03 13:43:28 -0600

    Delete - delete unused request and hook for user data

--- src/api/User/user.ts
+++
@@ -1,7 +0,0 @@
-import { axiosClient } from "@/config/axios";
-import { User } from "@/utils/types/userTypes";
-
-export const getCurrentUserRequest = async (id: number): Promise<User> => {
-  const { data } = await axiosClient.get(`/users/${id}`);
-  return data;
-};
